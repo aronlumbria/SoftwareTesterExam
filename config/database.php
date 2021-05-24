2676b741762d619b5a8b37f31751749ded6cb32d
Mike Dalisay <mike@codalify.com>
2021-05-24 11:13:37 +0800

    removed database name and renamed sql file

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@ class Database{
 
 	// specify your own database credentials
 	private $host = "localhost";
-	private $db_name = "php_oop_crud_level_1";
+	private $db_name = "";
 	private $username = "root";
 	private $password = "";
 	public $conn;
